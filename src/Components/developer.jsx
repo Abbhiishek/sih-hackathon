612f22cc2cb8e1809565266089668f619304b59d
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-09 14:58:06 +0530

    developer section added

---
+++ src/Components/developer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+export default function Developer() {
+
+    return(
+
+
+        <div>
+            
+            <h1>Hello this is Developers Page. </h1>
+        </div>
+    )
+
+}
