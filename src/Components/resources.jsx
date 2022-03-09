018b0ab1b39a53154176452774b563b7c7c870cd
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-09 14:58:36 +0530

    resources added

---
+++ src/Components/resources.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function App() {
+
+    return(
+
+        <div>
+            <div Section="Resources">
+                <div className="container">
+                    
+                    <h1>this is resourcesd</h1>
+                    
+                    
+                    </div>
+
+
+
+
+                </div>
+        </div>
+    )
+}
