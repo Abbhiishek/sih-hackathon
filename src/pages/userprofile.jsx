7c0b4bbb2ae5e0bb25a2ba0fa4e08744199af4f3
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-11 17:23:36 +0530

    created user profile

---
+++ src/pages/userprofile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {NavBar,About , Footer , Landingpage , Developer , Newsletter , Testimonial , AiChatBot , Overview} from '../Components/Home'
+
+export default function App() {
+
+return(
+    <div>
+                <h1>this is userprofile page </h1>
+    </div>
+)
+
+
+}
