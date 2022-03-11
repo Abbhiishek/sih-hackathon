36fe6d11bbba48c7720cc9a994c0f31f3382c83a
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-11 17:20:01 +0530

    added all current route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 
 import { Login } from './Components';
 import Home from './pages/home';
 import Dashboard from './pages/dashboard';
+import UserProfile from './pages/userprofile';
+import PageNotFound from './pages/404NOTFOUND';
 
 const App = () => {
   const navigate = useNavigate();
@@ -15,12 +17,18 @@ const App = () => {
   }, [Home,Dashboard]);
 
   return (
+    
     <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="/*" element={<Home />} />
-      <Route path="/dashboard/:userId" element={<Dashboard />} />
+      <Route path="login" element={<Login/>} />
+      <Route path="/" element={<Home/>} exact />
+      <Route path="/dashboard/:userId" element={<Dashboard/>} />
+      <Route path="/userprofile/:userId" element={<UserProfile/>} />
+      <Route path="/userprofile/:userId" element={<UserProfile/>} />
+      <Route path="/dashboard/:userId/userprofile/:userId" element={<UserProfile/>} />
+      <Route path="/:userId" element={<PageNotFound/>} />
       
     </Routes>
+    
   );
 };
 
