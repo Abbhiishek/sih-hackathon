cd6536f005cf650e2596cc664c8ccf5cc0755761
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-11 09:34:08 +0530

    main home page route

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import NavBar from '../Components/navbar'
-import AboutPage from '../Components/about'
-import Resources from '../Components/resources'
-import TeamPage from '../Components/developer'
-import FeaturesPage from '../Components/features'
-import NewsLetterPage from '../Components/newsletter'
-import Footer from '../Components/footer'
-export default function App() {
+import React from 'react';
+import {NavBar,About , Footer , Landingpage , Developer , Newsletter , Testimonial , AiChatBot , Overview} from '../Components/Home'
 
+
+
+export default function App() {
 
     return(
         <div>
         <NavBar/>
-        <AboutPage/>
-        <Resources/>
-        <TeamPage/>
-        <FeaturesPage/>
-        <NewsLetterPage/>
+        <Landingpage/>
+        <About/>
+        <Overview/>
+        <Developer/>
+        <Newsletter/>
+        <Testimonial/>
+        <AiChatBot/>
         <Footer/>
         </div>
         
