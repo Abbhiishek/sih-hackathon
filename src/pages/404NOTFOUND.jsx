309c8b395c7c16f20dc8526ec5e5762d0810e641
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-11 17:23:01 +0530

    added a custom page for error

---
+++ src/pages/404NOTFOUND.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function App(){
+    return(
+        <>
+        
+            <div className="container">
+                
+                
+             <h1 className="code pt-5">404 PAGE NOT FOUND</h1>
+             <a href="/">Get Redirected to The Main Page !</a> 
+                
+            </div>
+        
+        
+        
+        </>
+    )
+};
