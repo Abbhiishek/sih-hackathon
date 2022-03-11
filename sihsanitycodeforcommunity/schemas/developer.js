d4691f7401e221c39a17923ff1c08aa910a16d48
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-11 09:29:01 +0530

    Sanity Configuration

---
+++ sihsanitycodeforcommunity/schemas/developer.js
@@ -0,0 +1,30 @@
+export default {
+    name: 'developer',
+    title: 'Developer',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options:{
+            hotspot: true,
+        },
+      },
+      {
+        name: 'about',
+        title: 'About',
+        type: 'string',
+      },
+      {
+        name: 'social',
+        title: 'Social',
+        type: 'string',
+      },
+    ],
+  };
