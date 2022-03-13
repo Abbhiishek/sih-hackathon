be7894b85e8a66042e0d508534faeced1113359b
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-13 18:32:15 +0530

    improved dashboard navbar

--- src/Components/Dashboard/dashboard-Navbar.jsx
+++ src/Components/Dashboard/dashboard-Navbar.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 
 import aicte_logo_nav from '../../Images/aicte_logo_nav.png'
-import { Link , Route, Routes } from 'react-router-dom';
-import Home from '../../pages/home'
+import {  Route, Routes , useNavigate } from 'react-router-dom';
 import UserProfile from '../../pages/userprofile'
 import { userQuery } from '../../utils/data';
 import { client } from '../../client';
@@ -10,7 +9,13 @@ import { client } from '../../client';
 
 function NavBar() {
     const [user, setUser] = useState();
+    
     const scrollRef = useRef(null);
+
+    const [greet, setGreet] = useState();
+    const navigate = useNavigate();
+
+    
 
     const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
     
@@ -21,6 +26,21 @@ function NavBar() {
         client.fetch(query).then((data) => {
         setUser(data[0]);
         });
+    }, [userInfo]);
+
+
+    useEffect(() => {
+        let day = new Date();
+        let hr = day.getHours();
+        if (hr >= 0 && hr < 12) {
+            setGreet("Good Morning! ");
+        } else if (hr ===12) {
+            setGreet("Good Noon!");
+        } else if (hr >= 12 && hr <= 17) {
+            setGreet("Good Afternoon! ");
+        } else {
+            setGreet("Good Evening! ");
+        }
     }, []);
 
 
@@ -33,10 +53,10 @@ function NavBar() {
             <nav className="navbar sticky-top navbar-expand-lg navbar-light shadow-5-strong nav-background">
         <div className="container-fluid">
             
-            <a className="navbar-brand" href="#home" onClick={Home} >
-            <Link to={`home`}>
+            <a className="navbar-brand" href="#home" onClick={() => navigate('/') }>
+            
               <img src={aicte_logo_nav} alt="" width="380" height="73" class="d-inline-block align-text-top"/>
-            </Link>
+            
             </a>
             
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false">
@@ -45,7 +65,7 @@ function NavBar() {
             <div className="collapse navbar-collapse" id="navbarContent">
                 <ul className="navbar-nav mb-2 mb-lg-0 ms-auto gap-3">
                     <li className="nav-item">
-                        <p className="nav-link"><b>{user?.userName}</b></p>
+                        <p className="nav-link"><b>{greet}{user?.userName}</b></p>
                     </li>
                     <li className="nav-item">
                 
@@ -54,16 +74,14 @@ function NavBar() {
                     </li>
                     <li className="nav-item">
                         
-                        <Link to={`userprofile/${user?._id}`}>
-                                <img src={user?.image} alt="user-pic" className="rounded img-thumbnail"  width="50%"/>
-                        </Link>
+
+                                <img src={user?.image} alt="user-pic" onClick={() => navigate(`/userprofile/${user?._id}`)} className="rounded img-thumbnail"  width="50%"/>
+                        
     
                     </li>
                         <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
                             <Routes>
-                                <Route exact path="/Home" component={<Home/>} />
                                 <Route  path="/userprofile:userId" component={<UserProfile/>} />
-                                
                             </Routes>
                         </div>
                     
