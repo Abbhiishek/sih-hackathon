acd11691870fd3f961c688cb87fe421f71c2d5cc
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-03-25 23:41:29 +0530

    modified some chnages to overview section

--- src/Components/Home/features.jsx
+++ src/Components/Home/features.jsx
@@ -1,7 +1,8 @@
-import Scholorship_Photo from '../../Images/scholorship.jpg'
+import Scholorship_Photo from '../../Images/scholarship-.png'
 import Socials_Photo from '../../Images/social-media.png'
-import Events_Photo from '../../Images/events.jpg'
 import Aichat_Photo from '../../Images/Chatbot.png'
+import Scholorship_Gif from '../../Images/Scholorship-gif .gif'
+import AICHAT_Gif from '../../Images/AI-CHAT.gif'
 
 export default function App(){
 
@@ -24,18 +25,29 @@ export default function App(){
                     <h2>Scholorship </h2><br />
                     <p className="OverView-card-about"><em> Finding a Scholarship where you can apply can be a tedious process. <br /> But Don't worry! we got you covered. <br /> Our website allows you to easily browse through Scholarships by category.We make it simple for you to search for Scholarships that you qualify for. <br /> We do the work for you, All you need to do is register and complete your profile.</em></p>
                     </div>
+
+                <div className="col-lg-12">
+                <img src={Scholorship_Gif} alt=""  className="overview--photo" width="100%"/>
+                </div>
             </div>
 
             <div className="row overView--Socials  overview--card mb-4">
                 <div className="col-lg-6  ">
                     <h1>Social Hub</h1><br />
-                    <p className="OverView-card-about"><em> Finding a Scholarship where you can apply can be a tedious process. <br /> But Don't worry! we got you covered. <br /> Our website allows you to easily browse through Scholarships by category.We make it simple for you to search for Scholarships that you qualify for. <br /> We do the work for you, All you need to do is register and complete your profile.
+                    <p className="OverView-card-about">
+                        <em> Aicteo Provides you a platform to interact with your  
+                            <br /> But Don't worry! we got you covered. <br /> 
+                            Our website allows you to easily browse through Scholarships by category.We make it simple for you to search for Scholarships that you qualify for.
+                             <br /> We do the work for you, All you need to do is register and complete your profile.
                     </em>
                     </p>
                     </div>
                 <div className="col-lg-6">
                     <img src={Socials_Photo} alt=""  className="overview--photo" width="100%"/>
-                    </div>
+                </div>
+
+
+               
             </div>
             {/* <div className="row overView--Events  overview--card mb-4">
                 <div className="col-lg-6 pt-5 pb-5">
@@ -59,6 +71,16 @@ export default function App(){
                 </div>
                 <div className="col-lg-6 pt-5 pb-5">
                 <img src={Aichat_Photo} alt="" className="overview--photo" width="100%" />
+                </div>
+
+                <div className="col-lg-6">
+                   <img src={AICHAT_Gif} alt="" className="overview--photo" width="100%" />
+                </div>
+                <div className="col-lg-6">
+                      <div className="code">
+                       Aicteo Chatbot usses Python with Pytorch and Tensorflow to build a chatbot.
+                          
+                       </div> 
                 </div>
             </div>
             
